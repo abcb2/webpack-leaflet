e167ff7384eab2a8f2c39a655fcba50b7c674d80
abcb2 <kouji.wtnb@gmail.com>
2018-12-15 13:37:53 +0900

    step-2
    Signed-off-by: abcb2 <kouji.wtnb@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,20 @@
 let path = require('path');
 
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.png/,
+        use: [
+          'url-loader',
+        ],
+      },
+    ],
+  },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     port: 8080,
